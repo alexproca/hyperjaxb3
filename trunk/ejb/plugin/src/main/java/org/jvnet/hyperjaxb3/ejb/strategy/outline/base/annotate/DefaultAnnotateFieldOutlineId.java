eb06624e61d055507da52a4cb6d3cbe0e4068ad0
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-06-15 20:31:38 +0000

    Issue 131.
    git-svn-id: https://svn.java.net/svn/hj3~svn@49 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/outline/base/annotate/DefaultAnnotateFieldOutlineId.java
+++ trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/outline/base/annotate/DefaultAnnotateFieldOutlineId.java
@@ -2,13 +2,20 @@ package org.jvnet.hyperjaxb3.ejb.strategy.outline.base.annotate;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.TableGenerator;
+import javax.persistence.UniqueConstraint;
 
 import org.jvnet.annox.model.XAnnotation;
 import org.jvnet.annox.model.XAnnotationField;
+import org.jvnet.hyperjaxb3.annotation.util.AnnotationUtils;
 import org.jvnet.hyperjaxb3.ejb.schemas.customizations.Customizations;
 import org.jvnet.hyperjaxb3.ejb.strategy.outline.AnnotateFieldOutline;
 import org.jvnet.hyperjaxb3.ejb.strategy.outline.ProcessOutline;
@@ -21,6 +28,12 @@ public class DefaultAnnotateFieldOutlineId implements AnnotateFieldOutline {
 	public Collection<XAnnotation> process(ProcessOutline outlineProcessor,
 			FieldOutline fieldOutline, Options options) {
 
+		return createAnnotations(outlineProcessor, fieldOutline, options);
+	}
+
+	private Collection<XAnnotation> createAnnotations(
+			ProcessOutline outlineProcessor, FieldOutline fieldOutline,
+			Options options) {
 		final org.jvnet.hyperjaxb3.ejb.schemas.customizations.Id id = Customizations
 				.findCustomization(fieldOutline.getPropertyInfo(),
 						Customizations.ID_ELEMENT_NAME);
@@ -36,6 +49,11 @@ public class DefaultAnnotateFieldOutlineId implements AnnotateFieldOutline {
 		xannotations.addAll(outlineProcessor.getAnnotate()
 				.getAnnotateFieldOutlineColumn().process(outlineProcessor,
 						fieldOutline, options));
+
+		xannotations.addAll(createSequenceGenerator(outlineProcessor,
+				fieldOutline, options, id));
+		xannotations.addAll(createTableGenerator(outlineProcessor,
+				fieldOutline, options, id));
 		return xannotations;
 	}
 
@@ -70,4 +88,89 @@ public class DefaultAnnotateFieldOutlineId implements AnnotateFieldOutline {
 				"generator", generator);
 	}
 
+	public Collection<XAnnotation> createSequenceGenerator(
+			ProcessOutline outlineProcessor, FieldOutline fieldOutline,
+			Options options,
+			org.jvnet.hyperjaxb3.ejb.schemas.customizations.Id id) {
+
+		if (id.getSequenceGenerator() == null) {
+			return Collections.emptyList();
+		} else {
+			final com.sun.java.xml.ns.persistence.orm.SequenceGenerator cSequenceGenerator = id
+					.getSequenceGenerator();
+			final XAnnotation sequenceGenerator = new XAnnotation(
+					SequenceGenerator.class,
+					//
+					AnnotationUtils
+							.create("name", cSequenceGenerator.getName()),
+					//
+					AnnotationUtils.create("name", cSequenceGenerator
+							.getSequenceName()),
+					//
+					AnnotationUtils.create("name", cSequenceGenerator
+							.getInitialValue()),
+					//
+					AnnotationUtils.create("name", cSequenceGenerator
+							.getAllocationSize()));
+			return Collections.singletonList(sequenceGenerator);
+		}
+	}
+
+	public Collection<XAnnotation> createTableGenerator(
+			ProcessOutline outlineProcessor, FieldOutline fieldOutline,
+			Options options,
+			org.jvnet.hyperjaxb3.ejb.schemas.customizations.Id id) {
+
+		if (id.getTableGenerator() == null) {
+			return Collections.emptyList();
+		} else {
+			final com.sun.java.xml.ns.persistence.orm.TableGenerator cTableGenerator = id
+					.getTableGenerator();
+
+			final List<XAnnotation> uniqueConstraints = new LinkedList<XAnnotation>();
+			for (final com.sun.java.xml.ns.persistence.orm.UniqueConstraint cUniqueConstraint : cTableGenerator
+					.getUniqueConstraint()) {
+				final List<String> columnNames = cUniqueConstraint
+						.getColumnName();
+				final XAnnotation uniqueConstraint = new XAnnotation(
+						UniqueConstraint.class, AnnotationUtils.create(
+								"columnNames",
+								columnNames.toArray(new String[columnNames
+										.size()])));
+				uniqueConstraints.add(uniqueConstraint);
+
+			}
+
+			final XAnnotation tableGenerator = new XAnnotation(
+					TableGenerator.class,
+					//
+					AnnotationUtils.create("name", cTableGenerator.getName()),
+					//
+					AnnotationUtils.create("table", cTableGenerator.getTable()),
+					//
+					AnnotationUtils.create("catalog", cTableGenerator
+							.getCatalog()),
+					//
+					AnnotationUtils.create("schema", cTableGenerator
+							.getSchema()),
+					//
+					AnnotationUtils.create("pkColumnName", cTableGenerator
+							.getPkColumnName()),
+					//
+					AnnotationUtils.create("valueColumnName", cTableGenerator
+							.getValueColumnName()),
+					//
+					AnnotationUtils.create("pkColumnValue", cTableGenerator
+							.getPkColumnValue()),
+					//
+					AnnotationUtils.create("initialValue", cTableGenerator
+							.getInitialValue()),
+					//
+					AnnotationUtils.create("allocationSize", cTableGenerator
+							.getAllocationSize()), AnnotationUtils.create(
+							"uniqueConstraints", uniqueConstraints));
+
+			return Collections.singletonList(tableGenerator);
+		}
+	}
 }
