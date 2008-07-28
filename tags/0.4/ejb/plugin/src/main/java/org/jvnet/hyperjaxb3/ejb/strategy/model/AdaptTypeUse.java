813c8f1220203498f54147184b6a6e12613e1150
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-07-28 09:28:41 +0000

    Hudson automatic tag remove
    git-svn-id: https://svn.java.net/svn/hj3~svn@134 bcf56c81-c941-0410-8356-a2f62a4456c5

--- tags/0.4/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/model/AdaptTypeUse.java
+++
@@ -1,7 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.strategy.model;
-
-import com.sun.tools.xjc.model.TypeUse;
-
-public interface AdaptTypeUse extends PropertyInfoProcessor<TypeUse, ProcessModel>{
-
-}
--- tags/0.4/ejb/samples/build-1.5.bat
+++
@@ -1,4 +0,0 @@
-setlocal
-set JAVA_HOME=C:\Programme\Java\jdk1.5.0_06\
-call mvn -X -Dmaven.test.skip=true clean install >std 2>err
-endlocal
