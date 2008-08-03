610616c35c8969eab2cab9e88c9b8565a693f65f
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-08-03 19:36:30 +0000

    Hudson automatic tag remove
    git-svn-id: https://svn.java.net/svn/hj3~svn@152 bcf56c81-c941-0410-8356-a2f62a4456c5

--- tags/0.4/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/mapping/FieldOutlineMapping.java
+++
@@ -1,8 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.strategy.mapping;
-
-import org.jvnet.jaxb2_commons.strategy.FieldOutlineProcessor;
-
-public interface FieldOutlineMapping<T> extends
-		FieldOutlineProcessor<T, Mapping> {
-
-}
