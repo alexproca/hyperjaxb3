234582dc7e16934155ab23c8318c97df66deeb9e
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-08-05 14:53:54 +0000

    Hudson automatic tag add
    git-svn-id: https://svn.java.net/svn/hj3~svn@163 bcf56c81-c941-0410-8356-a2f62a4456c5

---
+++ tags/0.4/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/mapping/ManyToOneMapping.java
@@ -0,0 +1,70 @@
+package org.jvnet.hyperjaxb3.ejb.strategy.mapping;
+
+import java.util.Collection;
+
+import org.jvnet.jaxb2_commons.util.OutlineUtils;
+
+import com.sun.java.xml.ns.persistence.orm.JoinColumn;
+import com.sun.java.xml.ns.persistence.orm.JoinTable;
+import com.sun.java.xml.ns.persistence.orm.ManyToOne;
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.model.CPropertyInfo;
+import com.sun.tools.xjc.model.CTypeInfo;
+import com.sun.tools.xjc.model.nav.NType;
+import com.sun.tools.xjc.outline.FieldOutline;
+
+public class ManyToOneMapping extends AssociationMapping<ManyToOne> {
+
+	public ManyToOne process(Mapping context, FieldOutline fieldOutline,
+			Options options) {
+
+		final ManyToOne manyToOne = context.getCustomizing().getManyToOne(
+				fieldOutline);
+
+		createManyToOne$Name(context, fieldOutline, manyToOne);
+		createManyToOne$TargetEntity(context, fieldOutline, manyToOne);
+		createManyToOne$JoinTableOrJoinColumn(context, fieldOutline, manyToOne);
+		return manyToOne;
+	}
+
+	public void createManyToOne$Name(Mapping context,
+			FieldOutline fieldOutline, final ManyToOne manyToOne) {
+		manyToOne.setName(OutlineUtils.getPropertyName(fieldOutline));
+	}
+
+	public void createManyToOne$TargetEntity(Mapping context,
+			FieldOutline fieldOutline, final ManyToOne manyToOne) {
+
+		final CPropertyInfo propertyInfo = fieldOutline.getPropertyInfo();
+
+		final Collection<? extends CTypeInfo> types = propertyInfo.ref();
+
+		assert types.size() == 1;
+
+		final CTypeInfo type = types.iterator().next();
+
+		assert type instanceof NType;
+
+		final NType childClassInfo = (NType) type;
+
+		manyToOne.setTargetEntity(childClassInfo.fullName());
+
+	}
+
+	public void createManyToOne$JoinTableOrJoinColumn(Mapping context,
+			FieldOutline fieldOutline, ManyToOne manyToOne) {
+
+		if (manyToOne.getJoinColumn() != null
+				&& !manyToOne.getJoinColumn().isEmpty()) {
+			createJoinColumns(context, fieldOutline, manyToOne.getJoinColumn());
+		} else if (manyToOne.getJoinTable() != null) {
+			createJoinTable(context, fieldOutline, manyToOne.getJoinTable());
+		} else {
+			final JoinColumn joinColumn = new JoinColumn();
+			manyToOne.getJoinColumn().add(joinColumn);
+			createJoinColumns(context, fieldOutline, manyToOne.getJoinColumn());
+		}
+
+	}
+
+}
---
+++ tags/0.4/update-hyperjaxb3-versions.bat
@@ -0,0 +1,2 @@
+@echo off
+call update-versions ../hj3 %1 %2
