ece0c59c5a4c2111a470b2b86b57d17f1b4554f5
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-08-05 13:01:13 +0000

    Hudson automatic tag remove
    git-svn-id: https://svn.java.net/svn/hj3~svn@160 bcf56c81-c941-0410-8356-a2f62a4456c5

--- tags/0.4/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/strategy/model/PropertyInfoProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.strategy.model;
-
-import com.sun.tools.xjc.model.CPropertyInfo;
-
-public interface PropertyInfoProcessor<T, C> {
-
-	public T process(C context, CPropertyInfo propertyInfo);
-
-}
