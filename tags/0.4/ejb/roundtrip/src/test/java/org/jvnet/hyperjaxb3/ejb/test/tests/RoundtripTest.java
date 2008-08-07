173b06100d209065de96a1c42d3ca1c27d8ecfd4
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2008-08-07 11:52:50 +0000

    Hudson automatic tag remove
    git-svn-id: https://svn.java.net/svn/hj3~svn@170 bcf56c81-c941-0410-8356-a2f62a4456c5

--- tags/0.4/ejb/roundtrip/src/test/java/org/jvnet/hyperjaxb3/ejb/test/tests/RoundtripTest.java
+++
@@ -1,5 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.test.tests;
-
-public class RoundtripTest extends org.jvnet.hyperjaxb3.ejb.test.RoundtripTest {
-
-}
--- tags/0.4/ejb/tests/any/src/test/java/org/jvnet/hyperjaxb3/ejb/tests/any/RunAnyPlugin.java
+++
@@ -1,7 +0,0 @@
-package org.jvnet.hyperjaxb3.ejb.tests.any;
-
-import org.jvnet.hyperjaxb3.maven2.ejb.test.RunEjbHyperjaxb3Mojo;
-
-public class RunAnyPlugin extends RunEjbHyperjaxb3Mojo {
-
-}
