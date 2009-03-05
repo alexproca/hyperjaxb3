9458de216eec2f1f8ce989b7d9fb708e8333826f
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2009-03-05 09:49:02 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@239 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/ejb/tests/custom-naming/schema/src/test/java/org/jvnet/hyperjaxb3/ejb/tests/po/RunPoPlugin.java
+++ trunk/ejb/tests/custom-naming/schema/src/test/java/org/jvnet/hyperjaxb3/ejb/tests/po/RunPoPlugin.java
@@ -1,7 +1,20 @@
 package org.jvnet.hyperjaxb3.ejb.tests.po;
 
+import org.apache.maven.model.Dependency;
 import org.jvnet.hyperjaxb3.maven2.ejb.test.RunEjbHyperjaxb3Mojo;
+import org.jvnet.jaxb2.maven2.AbstractXJC2Mojo;
 
 public class RunPoPlugin extends RunEjbHyperjaxb3Mojo {
+
+	@Override
+	protected void configureMojo(AbstractXJC2Mojo mojo) {
+		final Dependency dependency = new Dependency();
+		dependency.setGroupId("org.jvnet.hyperjaxb3");
+		dependency
+				.setArtifactId("hyperjaxb3-ejb-tests-custom-naming-extension");
+		dependency.setVersion("0.5.3-SNAPSHOT");
+		mojo.setPlugins(new Dependency[] { dependency });
+		super.configureMojo(mojo);
+	}
 
 }
