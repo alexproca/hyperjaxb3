673d7e3ac272190fb01532b674a10b097f7781c8
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2009-07-08 19:24:10 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@285 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/model/HSeeAlso.java
+++ trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/model/HSeeAlso.java
@@ -1,3 +1,4 @@
+
 package org.jvnet.hyperjaxb3.model;
 
 import java.util.List;
@@ -6,15 +7,15 @@ import org.apache.commons.lang.Validate;
 
 public class HSeeAlso {
 
-	public final List<HClass> seeAlso;
+	public final List<LType> seeAlso;
 
-	public HSeeAlso(List<HClass> seeAlso) {
+	public HSeeAlso(List<LType> seeAlso) {
 		super();
 		Validate.notNull(seeAlso);
 		this.seeAlso = seeAlso;
 	}
 
-	public List<HClass> getSeeAlso() {
+	public List<LType> getSeeAlso() {
 		return seeAlso;
 	}
 }
