8e5aad6c1297f26772f8758e4aa2f972211f7fdb
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2010-04-20 22:06:29 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@393 bcf56c81-c941-0410-8356-a2f62a4456c5

---
+++ trunk/ejb/samples/customerservice-cxf/src/test/java/com/example/customerservice/test/CustomerServiceIT.java
@@ -0,0 +1,70 @@
+package com.example.customerservice.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.apache.cxf.tools.common.DataTypeAdapter;
+import org.hisrc.hifaces20.testing.webappenvironment.WebAppEnvironment;
+import org.hisrc.hifaces20.testing.webappenvironment.annotations.PropertiesWebAppEnvironmentConfig;
+import org.hisrc.hifaces20.testing.webappenvironment.testing.junit4.WebAppEnvironmentRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+import org.springframework.beans.factory.BeanFactoryUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.support.GenericXmlContextLoader;
+
+import com.example.customerservice.model.Customer;
+import com.example.customerservice.model.CustomerType;
+import com.example.customerservice.service.CustomerService;
+
+public class CustomerServiceIT {
+
+	@Rule
+	public MethodRule webAppEnvironmentRule = WebAppEnvironmentRule.INSTANCE;
+
+	@PropertiesWebAppEnvironmentConfig
+	public WebAppEnvironment webAppEnvironment;
+
+	@Test
+	public void checkCustomerService() throws Exception {
+
+		System.setProperty("baseUrl", webAppEnvironment.getBaseUrl());
+
+		ConfigurableApplicationContext applicationContext = new GenericXmlContextLoader()
+				.loadContext("classpath:com/example/customerservice/client/applicationContext.xml");
+
+		final CustomerService customerService = BeanFactoryUtils.beanOfType(
+				applicationContext, CustomerService.class);
+
+		final Customer originalCustomer = new Customer();
+
+		originalCustomer.setCustomerId(1);
+		originalCustomer.setName("Scott Tiger");
+		originalCustomer.getAddress().add("Hauptstr. 6");
+		originalCustomer.getAddress().add("76133 Karlsruhe");
+		originalCustomer.getAddress().add("Germany");
+		originalCustomer.setNumOrders(15);
+		originalCustomer.setRevenue(1234.56);
+		originalCustomer.setTest(BigDecimal.valueOf(7890));
+		originalCustomer.setBirthDate(DataTypeAdapter.parseDate("1970-01-01"));
+		originalCustomer.setType(CustomerType.BUSINESS);
+
+		customerService.updateCustomer(originalCustomer);
+
+		final List<Customer> customers = customerService
+				.getCustomersByName("Scott Tiger");
+
+		assertEquals(1, customers.size());
+
+		final Customer retrievedCustomer = customers.get(0);
+
+		assertEquals(originalCustomer, retrievedCustomer);
+
+		customerService.deleteCustomerById(retrievedCustomer.getCustomerId());
+
+	}
+}
