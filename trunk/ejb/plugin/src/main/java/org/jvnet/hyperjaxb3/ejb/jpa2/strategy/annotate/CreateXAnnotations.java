f0dde160d6820bab17899e41389f3794b81d352e
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2010-07-28 17:02:35 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@480 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/jpa2/strategy/annotate/CreateXAnnotations.java
+++ trunk/ejb/plugin/src/main/java/org/jvnet/hyperjaxb3/ejb/jpa2/strategy/annotate/CreateXAnnotations.java
@@ -1,13 +1,17 @@
 package org.jvnet.hyperjaxb3.ejb.jpa2.strategy.annotate;
 
 import java.util.Collection;
+import java.util.HashSet;
 
 import javax.persistence.AccessType;
+import javax.persistence.LockModeType;
 
 import org.jvnet.annox.model.XAnnotation;
+import org.jvnet.annox.model.XAnnotationField;
 import org.jvnet.hyperjaxb3.annotation.util.AnnotationUtils;
 
 import com.sun.java.xml.ns.persistence.orm.Basic;
+import com.sun.java.xml.ns.persistence.orm.CascadeType;
 import com.sun.java.xml.ns.persistence.orm.Embeddable;
 import com.sun.java.xml.ns.persistence.orm.Embedded;
 import com.sun.java.xml.ns.persistence.orm.EmbeddedId;
@@ -16,6 +20,7 @@ import com.sun.java.xml.ns.persistence.orm.Id;
 import com.sun.java.xml.ns.persistence.orm.ManyToMany;
 import com.sun.java.xml.ns.persistence.orm.ManyToOne;
 import com.sun.java.xml.ns.persistence.orm.MappedSuperclass;
+import com.sun.java.xml.ns.persistence.orm.NamedQuery;
 import com.sun.java.xml.ns.persistence.orm.OneToMany;
 import com.sun.java.xml.ns.persistence.orm.OneToOne;
 import com.sun.java.xml.ns.persistence.orm.SequenceGenerator;
@@ -25,6 +30,13 @@ import com.sun.java.xml.ns.persistence.orm.Version;
 public class CreateXAnnotations extends
 		org.jvnet.hyperjaxb3.ejb.strategy.annotate.CreateXAnnotations {
 
+	public XAnnotation createAccess(String access) {
+		return access == null ? null : new XAnnotation(
+				javax.persistence.Access.class, AnnotationUtils.create("value",
+						AccessType.valueOf(access)));
+
+	}
+
 	public XAnnotation createCacheable(Boolean cacheable) {
 		return cacheable == null ? null :
 		//
@@ -33,13 +45,6 @@ public class CreateXAnnotations extends
 						AnnotationUtils.create("value", cacheable)
 				//
 				);
-	}
-
-	public XAnnotation createAccess(String access) {
-		return access == null ? null : new XAnnotation(
-				javax.persistence.Access.class, AnnotationUtils.create("value",
-						AccessType.valueOf(access)));
-
 	}
 
 	@Override
@@ -117,11 +122,64 @@ public class CreateXAnnotations extends
 	}
 
 	@Override
+	public XAnnotation createOneToMany(OneToMany cOneToMany) {
+		return cOneToMany == null ? null :
+		//
+				new XAnnotation(javax.persistence.OneToMany.class,
+				//
+						cOneToMany.getTargetEntity() == null ? null
+								: new XAnnotationField.XClass("targetEntity",
+										cOneToMany.getTargetEntity()),
+						//
+						AnnotationUtils.create("cascade",
+								getCascadeType(cOneToMany.getCascade())),
+						//
+						AnnotationUtils.create("fetch", getFetchType(cOneToMany
+								.getFetch())),
+						//
+						AnnotationUtils.create("mappedBy", cOneToMany
+								.getMappedBy()),
+						//
+						AnnotationUtils.create("orphanRemoval", cOneToMany
+								.isOrphanRemoval())
+				//
+				);
+	}
+
+	@Override
 	public Collection<XAnnotation> createOneToManyAnnotations(OneToMany source) {
 		final Collection<XAnnotation> annotations = super
 				.createOneToManyAnnotations(source);
 		return source == null ? annotations : annotations(annotations,
 				createAccess(source.getAccess()));
+	}
+
+	// 9.1.23
+	public XAnnotation createOneToOne(OneToOne cOneToOne) {
+		return cOneToOne == null ? null :
+		//
+				new XAnnotation(javax.persistence.OneToOne.class,
+				//
+						cOneToOne.getTargetEntity() == null ? null
+								: new XAnnotationField.XClass("targetEntity",
+										cOneToOne.getTargetEntity()),
+						//
+						AnnotationUtils.create("cascade",
+								getCascadeType(cOneToOne.getCascade())),
+						//
+						AnnotationUtils.create("fetch", getFetchType(cOneToOne
+								.getFetch())),
+						//
+						AnnotationUtils.create("optional", cOneToOne
+								.isOptional()),
+						//
+						AnnotationUtils.create("mappedBy", cOneToOne
+								.getMappedBy()),
+						//
+						AnnotationUtils.create("orphanRemoval", cOneToOne
+								.isOrphanRemoval())
+				//
+				);
 	}
 
 	@Override
@@ -164,16 +222,68 @@ public class CreateXAnnotations extends
 						AnnotationUtils.create("sequenceName", source
 								.getSequenceName()),
 						//
-						AnnotationUtils.create("catalog", source
-								.getCatalog()),
+						AnnotationUtils.create("catalog", source.getCatalog()),
 						//
-						AnnotationUtils.create("schema", source
-								.getSchema()),
+						AnnotationUtils.create("schema", source.getSchema()),
 						//
 						AnnotationUtils.create("initialValue", source
 								.getInitialValue()),
 						//
 						AnnotationUtils.create("allocationSize", source
 								.getAllocationSize()));
+	}
+
+	// 8.3.1
+	public XAnnotation createNamedQuery(NamedQuery source) {
+		return source == null ? null :
+		//
+				new XAnnotation(javax.persistence.NamedQuery.class,
+						//
+						AnnotationUtils.create("query", source.getQuery()),
+						//
+						AnnotationUtils.create("hint", createQueryHint(source
+								.getHint())),
+						//
+						AnnotationUtils.create("name", source.getName()),
+						AnnotationUtils.create("lockMode",
+								createLockMode(source.getLockMode()))
+
+				//
+				);
+
+	}
+
+	public LockModeType createLockMode(String lockMode) {
+		return lockMode == null ? null : LockModeType.valueOf(lockMode);
+	}
+
+	public Collection<javax.persistence.CascadeType> getCascadeType(
+			CascadeType cascade) {
+
+		if (cascade == null) {
+			return null;
+		} else {
+			final Collection<javax.persistence.CascadeType> cascades = new HashSet<javax.persistence.CascadeType>();
+
+			if (cascade.getCascadeAll() != null) {
+				cascades.add(javax.persistence.CascadeType.ALL);
+			}
+			if (cascade.getCascadeMerge() != null) {
+				cascades.add(javax.persistence.CascadeType.MERGE);
+			}
+			if (cascade.getCascadePersist() != null) {
+				cascades.add(javax.persistence.CascadeType.PERSIST);
+			}
+			if (cascade.getCascadeRefresh() != null) {
+				cascades.add(javax.persistence.CascadeType.REFRESH);
+			}
+			if (cascade.getCascadeRemove() != null) {
+				cascades.add(javax.persistence.CascadeType.REMOVE);
+			}
+			if (cascade.getCascadeDetach() != null) {
+				cascades.add(javax.persistence.CascadeType.DETACH);
+			}
+			return cascades;
+		}
 	}
 }
