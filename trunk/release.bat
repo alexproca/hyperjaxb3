4144a072977fbf6f62f3f5ff89da9f38040e8c5d
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2011-07-05 21:08:50 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@556 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/release.bat
+++ trunk/release.bat
@@ -1,6 +1,11 @@
 rem update versions
-call mvn clean install -Psamples
+rem call mvn clean install
+rem call mvn clean install -Psamples
+rem call mvn clean install -Ptests
+rem call mvn clean install -Ptemplates
+rem call mvn clean install -Ptutorials
 call mvn clean install -Ptests
+call mvn clean install -Psamples
 call mvn clean install -Ptemplates
 call mvn clean install -Ptutorials
 rem mvn scm:tag -Dtag=%1
