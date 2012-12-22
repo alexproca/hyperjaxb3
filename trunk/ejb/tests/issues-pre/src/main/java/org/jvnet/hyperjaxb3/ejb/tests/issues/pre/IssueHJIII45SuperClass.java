7d61ea442fe2a44ad97136e2f72e15bfacda805b
lexi <lexi@bcf56c81-c941-0410-8356-a2f62a4456c5>
2012-12-22 13:28:01 +0000

    git-svn-id: https://svn.java.net/svn/hj3~svn@608 bcf56c81-c941-0410-8356-a2f62a4456c5

--- trunk/ejb/tests/issues-pre/src/main/java/org/jvnet/hyperjaxb3/ejb/tests/issues/pre/IssueHJIII45SuperClass.java
+++ trunk/ejb/tests/issues-pre/src/main/java/org/jvnet/hyperjaxb3/ejb/tests/issues/pre/IssueHJIII45SuperClass.java
@@ -8,11 +8,18 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.PrePersist;
 import javax.xml.bind.annotation.XmlElement;
-
+import org.jvnet.jaxb2_commons.lang.Equals;
+import org.jvnet.jaxb2_commons.lang.EqualsStrategy;
+import org.jvnet.jaxb2_commons.lang.HashCode;
+import org.jvnet.jaxb2_commons.lang.HashCodeStrategy;
+import org.jvnet.jaxb2_commons.lang.JAXBEqualsStrategy;
+import org.jvnet.jaxb2_commons.lang.JAXBHashCodeStrategy;
+import org.jvnet.jaxb2_commons.locator.ObjectLocator;
+import org.jvnet.jaxb2_commons.locator.util.LocatorUtils;
 
 
 @MappedSuperclass
-public abstract class IssueHJIII45SuperClass implements Serializable{
+public abstract class IssueHJIII45SuperClass implements Serializable, Equals, HashCode{
 
     private static final long serialVersionUID = 7724857660567518243L;
 
@@ -42,6 +49,45 @@ public abstract class IssueHJIII45SuperClass implements Serializable{
 	
 	
 	
+    public boolean equals(ObjectLocator thisLocator, ObjectLocator thatLocator, Object object, EqualsStrategy strategy) {
+        if (!(object instanceof IssueHJIII45SuperClass)) {
+            return false;
+        }
+        if (this == object) {
+            return true;
+        }
+        final IssueHJIII45SuperClass that = ((IssueHJIII45SuperClass) object);
+        {
+            String lhsId;
+            lhsId = this.getId();
+            String rhsId;
+            rhsId = that.getId();
+            if (!strategy.equals(LocatorUtils.property(thisLocator, "id", lhsId), LocatorUtils.property(thatLocator, "id", rhsId), lhsId, rhsId)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean equals(Object object) {
+        final EqualsStrategy strategy = JAXBEqualsStrategy.INSTANCE;
+        return equals(null, null, object, strategy);
+    }
+
+    public int hashCode(ObjectLocator locator, HashCodeStrategy strategy) {
+        int currentHashCode = 1;
+        {
+            String theId;
+            theId = this.getId();
+            currentHashCode = strategy.hashCode(LocatorUtils.property(locator, "id", theId), currentHashCode, theId);
+        }
+        return currentHashCode;
+    }
+
+    public int hashCode() {
+        final HashCodeStrategy strategy = JAXBHashCodeStrategy.INSTANCE;
+        return this.hashCode(null, strategy);
+    }
 
 
 }
